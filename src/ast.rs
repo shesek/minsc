3375a598c88b04c92f4580452f0f4a8163a20fa7
Nadav Ivgi <nadav@shesek.info>
2020-07-01 04:31:00 +0300

    Rename Identifier -> Ident

--- src/ast.rs
+++ src/ast.rs
@@ -1,14 +1,14 @@
 
-pub type Identifier = String;
+pub type Ident = String;
 
 #[derive(Debug)]
 pub enum Expr {
-  FnCall(Identifier, Vec<Expr>),
-  FnDef(Identifier, Vec<String>, Box<Expr>),
-  Assign(Identifier, Box<Expr>),
+  FnCall(Ident, Vec<Expr>),
+  FnDef(Ident, Vec<String>, Box<Expr>),
+  Assign(Ident, Box<Expr>),
   Or(Vec<Expr>),
   And(Vec<Expr>),
   Block(Vec<Expr>),
   Value(String), // plain value (hex pubkeys, xpubs, locktimes, etc)
-  Var(Identifier), // plain value (hex pubkeys, xpubs, locktimes, etc)
+  Var(Ident), // plain value (hex pubkeys, xpubs, locktimes, etc)
 }
