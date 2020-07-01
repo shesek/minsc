cfd11f0d19919b5e507492b3bfeec396e6dfaf1d
Nadav Ivgi <nadav@shesek.info>
2020-07-01 05:16:18 +0300

    Indent

--- src/ast.rs
+++ src/ast.rs
@@ -1,14 +1,13 @@
-
 pub type Ident = String;
 
 #[derive(Debug)]
 pub enum Expr {
-  FnCall(Ident, Vec<Expr>),
-  FnDef(Ident, Vec<String>, Box<Expr>),
-  Assign(Ident, Box<Expr>),
-  Or(Vec<Expr>),
-  And(Vec<Expr>),
-  Block(Vec<Expr>),
-  Value(String), // plain value (hex pubkeys, xpubs, locktimes, etc)
-  Var(Ident), // plain value (hex pubkeys, xpubs, locktimes, etc)
+    FnCall(Ident, Vec<Expr>),
+    FnDef(Ident, Vec<Ident>, Box<Expr>),
+    Assign(Ident, Box<Expr>),
+    Or(Vec<Expr>),
+    And(Vec<Expr>),
+    Block(Vec<Expr>),
+    Value(String), // plain value (hex pubkeys, xpubs, locktimes, etc)
+    //Var(Ident),    // plain value (hex pubkeys, xpubs, locktimes, etc)
 }
--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,4 @@
 pub fn concat<T>(mut list: Vec<T>, val: T) -> Vec<T> {
-  list.push(val);
-  list 
+    list.push(val);
+    list
 }
