6924522e86856578658b18ae9e29b98e583a595b
Nadav Ivgi <nadav@shesek.info>
2020-08-03 11:54:35 +0300

    Update federated pegin example

--- web/js/default-code.js
+++ web/js/default-code.js
@@ -1,6 +1,6 @@
 export default
 
-`// Liquid-like federated pegin, with emergency recovery keys
+`// Liquid-like federated pegin with emergency recovery keys
 
 $federation = 4 of [ pk(A), pk(B), pk(C), pk(D), pk(E) ]; 
 $recovery = 2 of [ pk(F), pk(G), pk(H) ];
