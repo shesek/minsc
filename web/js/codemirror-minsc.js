f942c48ac92b4a76eae9b93a96cf5baf02e9ba59
Nadav Ivgi <nadav@shesek.info>
2024-05-13 07:07:48 +0300

    playground: Update CodeMirror syntax highlighting mode

--- web/js/codemirror-minsc.js
+++ web/js/codemirror-minsc.js
@@ -5,11 +5,11 @@ CodeMirror.defineSimpleMode("minsc",{
     // Time durations
     {regex: /\d+(\.\d+)?\s+(years?|months?|weeks?|days?|hours?|min(?:utes?|s)|seconds?)\b/, token: "number"},
     // BTC amounts
-    {regex: /(?:BTC|mBTC|uBTC|bits?|satoshis?|sats?|msats?)\b/, token: "number"},
+    {regex: /\d+(?:\.\d+)?\s+(?:BTC|mBTC|uBTC|bits?|satoshis?|sats?|msats?)\b/, token: "number"},
     // Block height durations
     {regex: /\d+\s+blocks?\b/, token: "number"},
     // Dates
-    {regex: /\d{4}-\d{1,2}-\d{1,2}(\s+\d{1,2}:\d{1,2})?\b/, token: "number"},
+    {regex: /\d{4}-\d{1,2}-\d{1,2}T(\d{1,2}:\d{1,2}(:\d{1,2})?Z?)?\b/, token: "number"},
     // Xpubs, single keys & hashes
     {regex: /\b([a-fA-F0-9]{8}|[a-fA-F0-9]{40,130}|0x[a-fA-F0-9]*|[xt]pub[0-9a-zA-Z]{100,120})\b/, token: "number"},
     // Addresses (Bech32 & Base58check)
@@ -20,9 +20,11 @@ CodeMirror.defineSimpleMode("minsc",{
     {regex: /"(?:[^\\]|\\.)*?(?:"|$)/, token: "string"},
 
     // Keywords
-    {regex: /\b(of|return|let|heightwise|likely|if|then|else|true|false)\b/, token: "keyword"},
+    {regex: /\b(of|return|let|if|then|else|_)\b/, token: "keyword"},
+    {regex: /\b(true|false|null|default|int|float|str|bytes|likely)\b/, token: "builtin"},
 
     // Function definition
+    {regex: /\b(fn)(\s+)([$a-zA-Z_][$a-zA-Z_0-9]*(?:::[a-zA-Z0-9_$]+)*)(\(\[native\]\))/, token: ["keyword", null, "def", "def"]},
     {regex: /\b(fn)(\s+)([$a-zA-Z_][$a-zA-Z_0-9]*(?:::[a-zA-Z0-9_$]+)*)\b/, token: ["keyword", null, "def"]},
 
     // Assignment
@@ -32,19 +34,21 @@ CodeMirror.defineSimpleMode("minsc",{
     {regex: /\/\/.*/, token: "comment"},
 
     // Infix operators
-    {regex: /[-+\/*<>!;@]|[=!<>]=|&&|\|\|/, token: "operator"},
+    {regex: /[-+\/*<>!;@]|[=!<>]=|&&|\|\||\|/, token: "operator"},
 
     // Function calls
-    {regex: /([$a-zA-Z_][$a-zA-Z_0-9]*(?:::[a-zA-Z0-9_$]+)*)\s*(\()/, token: ["atom", null]},
+    {regex: /([$a-zA-Z_][$a-zA-Z_0-9]*(?:::[a-zA-Z0-9_$]+)*)\s*(\()/, token: ["atom", null], indent: true},
 
     // Variables
-    {regex: /[A-Z$][A-Z0-9_]+\b/, token: "variable-2"},
+    {regex: /[A-Z$][A-Z0-9_]+\b/, token: "variable-2"}, // different look for all-caps identifiers, typically OP_CODES
     {regex: /[$a-zA-Z_][$a-zA-Z_0-9]*(?:::[a-zA-Z0-9_$]+)*\b/, token: "variable-3"},
 
     // Numeric array index
     {regex: /\.\d+\b/, token: "property"},
 
     // Indentation for { [ (
+    //{regex: /[\{\[\(]/, indent: true},
+    //{regex: /[\}\]\)]/, dedent: true}
     {regex: /[\{\[\(]/, indent: true},
     {regex: /[\}\]\)]/, dedent: true}
   ],
