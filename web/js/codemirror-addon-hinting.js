e7807b093a02aa45bc71d92f23808d8c69eee68d
Nadav Ivgi <nadav@shesek.info>
2024-05-19 17:39:58 +0300

    playground: Display function arguments when auto-completing stdlib functions
    Available for Minsc-defined functions only, not for native Rust ones.

--- web/js/codemirror-addon-hinting.js
+++ web/js/codemirror-addon-hinting.js
@@ -4,57 +4,85 @@ const WORD = /[\w$:]+/, RANGE = 500
 
 // Combines results from a pre-defined wordlist and using words in the document (anyhint)
 CodeMirror.registerHelper("hint", "multihint", (cm, options) => {
-  const word = options.word || WORD
+  const wordRe = options.word || WORD
   const wordlist = options.wordlist
   const cur = cm.getCursor(), currLine = cm.getLine(cur.line)
 
   // Don't complete at the middle of words
-  if (options.wordEndOnly && word.test(currLine.charAt(cur.ch)))
+  if (options.wordEndOnly && wordRe.test(currLine.charAt(cur.ch)))
     return;
 
   // Find auto-completed word
   let start = cur.ch
-  while (start && word.test(currLine.charAt(start - 1))) --start;
+  while (start && wordRe.test(currLine.charAt(start - 1))) --start;
   const currWord = start < cur.ch ? currLine.slice(start, cur.ch) : '';
 
   if (options.minSearchLen != null && currWord.length < options.minSearchLen)
     return;
 
-  // Get hints from the built-in autocomplete wordlist
-  const listhints = currWord ? wordlist.filter(kw => kw.indexOf(currWord) != -1) : wordlist
-  // Get hints from elsewhere in the document
-  const anyhints = currWord ? anyword(cm, options, currWord) : []
-  const words = new Set([ ...listhints, ...anyhints ])
+  const listhints = wordlistHints(wordlist, currWord)
+  const anyhints = anywordHints(cm, options, currWord, listhints)
+  const hints = [ ...listhints, ...anyhints ]
+    .sort(matchSorter(currWord))
 
-  // Don't suggest the word being typed as the only suggestion
-  if (words.size == 1) words.delete(currWord)
+  // Don't suggest the word being typed as the only suggestion,
+  // unless its a function with a useful displayText that includes the arguments.
+  if (hints.length == 1 && hints[0].text == currWord && !hints[0].displayText)
+    return;
 
-  if (words.size > 0 && (!options.displayIfLess || words.size < options.displayIfLess)) {
+  if (hints.length > 0 && (!options.displayIfLess || hints.length < options.displayIfLess)) {
     return {
-      list: Array.from(words).sort(matchSorter(currWord)),
+      list: hints,
       from: CodeMirror.Pos(cur.line, start),
-      to: cur, // CodeMirror.Pos(cur.line),
+      to: cur,
     }
   }
 })
 
-// Show matches that start with currWord first, then by name
+// Prefer matches that start with currWord first, then functions over variables, then by name
 const matchSorter = currWord => (a, b) => {
-  const aStarts = a.startsWith(currWord), bStarts = b.startsWith(currWord)
+  const aStarts = a.text.startsWith(currWord), bStarts = b.text.startsWith(currWord)
+      , aIsFunc = !!a.displayText, bIsFunc = !!b.displayText
   return aStarts != bStarts ? (aStarts ? -1 : 1)
-    : a < b ? -1 : 1
+       : aIsFunc != bIsFunc ? (aIsFunc ? -1 : 1)
+       : a.text < b.text ? -1 : 1
 }
 
+// Get hints from the built-in autocomplete list of stdlib variables/functions
+function wordlistHints(wordlist, currWord) {
+  wordlist._hints = wordlist._hints || [
+    ...wordlist.vars.map(v => ({ text: v })),
+    ...wordlist.funcs.map(([ name, args ]) => ({
+      text: name,
+      displayText: `${name}(${args != '[native]' ? args : '..'})`,
+      hint: applyFuncHint
+    }))
+  ]
 
+  // Returns everything when completing an empty string
+  return !currWord ? wordlist._hints :
+    wordlist._hints.filter(w => w.text.indexOf(currWord) != -1)
+}
+
+function applyFuncHint(cm, data, completion) {
+    const from = completion.from || data.from, to = completion.to || data.to
+    // Add parenthesis and move the cursor into them
+    cm.replaceRange(completion.text + '()', from , to, "complete")
+    cm.setCursor({ line: to.line, ch: cm.getCursor().ch-1 })
+}
+
+// Get hints from words found in the document
 // Based on https://codemirror.net/5/addon/hint/anyword-hint.js
-function anyword(cm, options, currWord) {
+function anywordHints(cm, options, currWord, found) {
+  if (!currWord) return []
+
   const wordRe = options.word || WORD
       , range = options.range || RANGE
       , minLen = options.minAnyhintLen
       , maxLen = options.maxAnyhintLen || 40
   const cur = cm.getCursor()
   const re = new RegExp(wordRe.source, "g");
-  const matches = []
+  const matches = new Set, foundWords = new Set(found.map(f => f.text))
   for (let dir = -1; dir <= 1; dir += 2) {
     let line = cur.line, endLine = Math.min(Math.max(line + dir * range, cm.firstLine()), cm.lastLine()) + dir;
     for (; line != endLine; line += dir) {
@@ -65,12 +93,13 @@ function anyword(cm, options, currWord) {
         word = word.replace(/^:+|:+$/, '')
         if (minLen != null && word.length < minLen) continue;
         if (word.length > maxLen) continue;
+        if (foundWords.has(word)) continue;
         if (!/[a-zA-Z$]/.test(word) || /^0x[a-f0-9]+$/.test(word)) continue;
         if (!currWord || word.lastIndexOf(currWord, 0) == 0) {
-          matches.push(word);
+          matches.add(word);
         }
       }
     }
   }
-  return matches
+  return Array.from(matches).map(word => ({ text: word }))
 }
--- web/update-wordlist.sh
+++ web/update-wordlist.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-#wasm-pack build --target nodejs --out-dir $PWD/../node-pkg
+wasm-pack build --target nodejs --out-dir $PWD/../node-pkg
 
 node -p '
-  const ignored = [ "true", "false", "null" ]
-  const words = require("../node-pkg/minsc.js")
-    .run("map(env(0), |$kv| $kv.0)");
-  JSON.stringify(JSON.parse(words)
-    .filter(w => w.length >= 2 && !w.startsWith("_") && !ignored.includes(w)))
+  const [ vars, funcs ] = JSON.parse(require("../node-pkg/minsc.js").run(`
+    [
+      env(0) | filter(|$kv| !isFunction($kv.1)) | map(|$kv| $kv.0),
+      env(0) | filter(|$kv| isFunction($kv.1)) | map(|$kv| $kv.0 + " " + str($kv.1)),
+    ]
+  `));
+  JSON.stringify({
+    vars: vars.filter(v => v.length >= 2 && !v.startsWith("_")),
+    funcs: funcs.filter(v => !v.startsWith("_")).map(def => [
+        def.slice(0, def.indexOf(" ")),
+        def.slice(def.indexOf("(")+1, def.length-1).replace(/\$\$/g, "$")
+    ]),
+  }, null, 2)
 ' > js/stdlib-wordlist.json
