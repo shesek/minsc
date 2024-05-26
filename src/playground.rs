d4002a0c82dcc635122b93f10993af0fb62c7fb5
Nadav Ivgi <nadav@shesek.info>
2024-05-27 00:40:55 +0300

    playground: Remove some built-in playground variables, update others

--- src/playground.rs
+++ src/playground.rs
@@ -125,22 +125,13 @@ lazy_static! {
         J = pubkey(0x03d2810d442a784e93133760af5ac05e4eb72364a3257e5a5eafc618ccb15e580a);
         K = pubkey(0x03a81dca4cde2edf3d193e2b2446b40aa04f33dd11a4599c7fa55415fc274f0f70);
         L = pubkey(0x029e5de3f2391700fdb5f45aa5db40b953de8bd4a147663b1cd89aa0703a0c2fcf);
-        user_pk = pubkey(0x03c620141755e90c86ec35fe57594e0b4b1a32f09f15bc0a43b06f9feb71c1b06c);
-        service_pk = pubkey(0x02f8b2c15f9e301d7e46169a35088724cbcb264f678d628d615c38ee964f836245);
-        buyer_pk = pubkey(0x03829e91bb8d4df87fea147f98ef5d3e71c7c26204a5ed5de2d1d966938d017ac2);
-        seller_pk = pubkey(0x0215152236dd9f518dd2bba50487857b98bdb4778c3618780a25a0cbc660092185);
-        arbiter_pk = pubkey(0x0203bc5458e2b77b5f5a68a738a57bee0271a27e603100c4110533bf8811c19e2e);
-        ceo_pk = pubkey(0x03e9035b99913ea072be74032489f7d20725ae496f8809b1c1924dbeacf590c5ed);
-        desktop_pk = pubkey(0x02e0e913c8e67ee002ed4a877a54722b0483f999ad49111081318f204f1a470c58);
-        mobile_pk = pubkey(0x02065bf89fb085e06188a885fc191e25469ebd2868b160bd525778eedbe2f987cf);
+
         $alice = xpub68Gmy5EdvgibQVfPdqkBBCHxA5htiqg55crXYuXoQRKfDBFA1WEjWgP6LHhwBZeNK1VTsfTFUHCdrfp1bgwQ9xv5ski8PX9rL2dZXvgGDnw/0;
         $bob = xpub69tT6QWuMwV9bppMzxDX39hz6vcwsgK6YE4gR5cA4auaHsX6dR7uCu566h2WXEGjQe8B89PUJEwsaTJZiDTmLbGVPDDz9JjohA46jUEPvtb;
-        $charlie = xpub6BCmnsYVHYpxh4gPrwWXbYZwGsDHoGP69rQR7EQkWDNKJDuCqhSJXBRsTxiap6fsCmG2YNUVjn3WhpqvtRcwuMX5HyFdvuEwghKjmWMaGrA;
+        $charlie = xpub6BCmnsYVHYpxh4gPrwWXbYZwGsDHoGP69rQR7EQkWDNKJDuCqhSJXBRsTxiap6fsCmG2YNUVjn3WhpqvtRcwuMX5HyFdvuEwghKjmWMaGrA/3/*;
 
-        XPUB = xpub68Gmy5EdvgibQVfPdqkBBCHxA5htiqg55crXYuXoQRKfDBFA1WEjWgP6LHhwBZeNK1VTsfTFUHCdrfp1bgwQ9xv5ski8PX9rL2dZXvgGDnw/9/0;
-        H = 0x01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b;
-        H1 = 0x4355a46b19d348dc2f57c046f8ef63d4538ebb93;
-        H2 = 0x53c234e5e8472b6ac51c1ae1cab3fe06fad053be;
+        $H256 = 0x01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b;
+        $H160 = 0x4355a46b19d348dc2f57c046f8ef63d4538ebb93;
     "#
     .parse()
     .unwrap();
