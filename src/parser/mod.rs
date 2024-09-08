2caab845f313b7a1fd994962dd6d8f22e49f2615
Nadav Ivgi <nadav@shesek.info>
2024-09-08 10:25:31 +0300

    parser: Update LALRPOP to v0.21
    Switch to lalrpop::process_src() as per the instructions on the changelog:
    
    > lalrpop::process_src() is now the recommended function to use in build.rs files.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -9,7 +9,7 @@ pub use ast::{Expr, Ident, Stmt, Stmts};
 lalrpop_mod!(
     #[allow(clippy::all)]
     pub grammar,
-    "/src/parser/grammar.rs"
+    "/parser/grammar.rs"
 );
 
 pub type Library = Stmts;
