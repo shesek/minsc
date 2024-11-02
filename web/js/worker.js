f5dcf962bea1566cdd6a3b787001fda2f8c92461
Nadav Ivgi <nadav@shesek.info>
2024-11-02 23:29:06 +0200

    playground: Only process the latest Web Worker request

--- web/js/worker.js
+++ web/js/worker.js
@@ -2,21 +2,18 @@ let minsc, pending
 
 import('../pkg/index.js').then(module => {
   minsc = module
-  if (pending) {
-    run(pending)
-    pending = null
-  }
+  if (pending) run(pending)
 }).catch(console.error)
 
 addEventListener('message', ({ data: req }) => {
-  if (!minsc) {
-    pending = req
-  } else {
-    run(req)
-  }
+  // Wait for the next event loop 'tick' before processing, so that we first read all queued messages
+  // (multiple could've been accumulated while we were `run()`ing) and only process the latest one
+  if (minsc && !pending) setTimeout(() => run(pending), 0)
+  pending = req
 })
 
 function run({ code, network }) {
+  pending = null
   try {
     const result = minsc.run_playground(code, network)
     postMessage({ result })
