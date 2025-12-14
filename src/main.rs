09465b8bf2ca50d7aff123e01d113d6ac05bf08e
Nadav Ivgi <nadav@shesek.info>
2025-12-15 00:46:52 +0200

    cli: Add --repr and --explicit options

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,48 @@
-use minsc::{eval, parse, Error, PrettyDisplay, Value};
+use minsc::{eval, parse, Error, ExprRepr, PrettyDisplay, Value};
 use std::io::{self, Read};
 use std::{env, fs, process::ExitCode};
 
 fn main_() -> Result<ExitCode, Error> {
     let mut args = env::args();
-    let input = args.nth(1).unwrap_or_else(|| "-".into());
-    let print_ast = args.next().is_some_and(|arg| arg == "--ast");
+    let input = args.nth(1);
+    let rest = args.collect::<Vec<_>>();
+    let print_ast = rest.iter().any(|arg| arg == "--ast");
+    let repr_res = rest.iter().any(|arg| arg == "--repr");
+    let explicit_res = rest.iter().any(|arg| arg == "--explicit");
 
     let mut code = String::new();
-    match &*input {
-        "-" => io::stdin().read_to_string(&mut code)?,
-        file => fs::File::open(file)?.read_to_string(&mut code)?,
+    match input.as_deref() {
+        None | Some("-") => io::stdin().read_to_string(&mut code)?,
+        Some(path) => fs::File::open(path)?.read_to_string(&mut code)?,
     };
 
-    Ok(if print_ast {
+    if print_ast {
         println!("{:#?}", parse(&code)?);
-        ExitCode::SUCCESS
-    } else {
-        let res = eval(&code)?;
+        return Ok(ExitCode::SUCCESS);
+    }
+
+    let res = eval(&code)?;
+
+    if repr_res {
+        print!("{}", res.repr_str());
+    } else if !explicit_res {
+        // Pretty display optimized for console output
         match &res {
             // Unnecessary to print return values of `true`, the SUCCESS exit code is enough
             Value::Bool(true) => {}
-            // Print raw strings with no quoting
-            Value::String(string) => print!("{}", string),
+            // Print raw strings in multi-line with no quoting
+            Value::String(string) => println!("{}", string),
             _ => println!("{}", res.pretty_multiline()),
         }
-        if res == Value::Bool(false) {
-            ExitCode::FAILURE
-        } else {
-            ExitCode::SUCCESS
-        }
+    } else {
+        // Pretty display with quoted strings and explicit `true`, for non-console use (by other programs)
+        print!("{}", res.pretty_multiline())
+    }
+
+    Ok(if res == Value::Bool(false) {
+        ExitCode::FAILURE
+    } else {
+        ExitCode::SUCCESS
     })
 }
 
